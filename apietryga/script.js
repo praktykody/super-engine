d6202949b2fc2f3b5639471b1530e00507915f1a
apietryga <antek.pietryga@gmail.com>
2023-04-01 14:08:22 +0200

    ternary if

--- apietryga/script.js
+++ apietryga/script.js
@@ -2,14 +2,12 @@ let slide = 0
 function changeSlide(index){
 	const imgs = document.querySelectorAll(".wrapper img")
 	imgs.forEach((img, imgIndex) => {
-
-		console.log(index, imgIndex, index % 3)
-
-		if(index % 3 == imgIndex){
-			img.style.opacity = 1
-		}else{
-			img.style.opacity = 0
-		}
-
+		img.style.opacity = Math.abs(index) % 3 == imgIndex ? 1 : 0;
+		// if(Math.abs(index) % 3 == imgIndex){
+		// 	img.style.opacity = 1
+		// }else{
+		// 	img.style.opacity = 0
+		// }
 	})
 }
+changeSlide(slide)
