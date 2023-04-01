be52ea7986c689967cf7a76b48583e03abe7dc69
Karolina81 <karolinaukolowa81@gmail.com>
2023-04-01 14:51:57 +0200

    sobota

--- Karolina81/script.js
+++ Karolina81/script.js
@@ -2,15 +2,9 @@ let slide = 0
 function changeSlide(index){
     const imgs = document.querySelectorAll(".wrapper img")
     imgs.forEach((img, imgIndex) => {
-        console.log(index, imgIndex, index % 3)
-        if(index % 3 ==imgIndex){
-            img.style.opacity = 1
-        }else{
-            img.style.opacity = 0
-
-        }
-    
+        img.style.opacity = Math.abs(index) % 3 == imgIndex ? 1:0;
 
     })
     
 }
+changeSlide(slide)
