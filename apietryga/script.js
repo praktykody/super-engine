26a2a388ac9e0970117faa38f62d436f30243da3
apietryga <antek.pietryga@gmail.com>
2023-04-01 13:51:34 +0200

    right arrow working

---
+++ apietryga/script.js
@@ -0,0 +1,15 @@
+let slide = 0
+function changeSlide(index){
+	const imgs = document.querySelectorAll(".wrapper img")
+	imgs.forEach((img, imgIndex) => {
+
+		console.log(index, imgIndex, index % 3)
+
+		if(index % 3 == imgIndex){
+			img.style.opacity = 1
+		}else{
+			img.style.opacity = 0
+		}
+
+	})
+}
