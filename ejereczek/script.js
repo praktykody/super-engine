4204c582df04b5657ffb80ee02ea7d6e7f69a905
ejereczek <edytajereczek93@gmail.com>
2023-04-01 14:10:34 +0200

    img opacity changed for shorter code

--- ejereczek/script.js
+++ ejereczek/script.js
@@ -5,16 +5,20 @@
 
 let slide = 0
 function changeSlide(index){
-    console.log(index)
+    console.log(index,Math.abs(index) % 3)
     const imgs = document.querySelectorAll(".wrapper img")
     // console.log(index, imgs)
     imgs.forEach((img, imgIndex) => {
-        console.log(index, imgIndex, index % 3)
-        if(index % 3 == imgIndex){
-            img.style.opacity=1
-        }else{
-            img.style.opacity=0
-        }
+
+        img.style.opacity = Math.abs(index) % 3 == imgIndex ? 1 : 0;
+        // if(Math.abs(index) % 3 == imgIndex){
+        //     img.style.opacity=1
+        // }else{
+        //     img.style.opacity=0
+        // }
+
+
 
     })
 }
+changeSlide(slide)
